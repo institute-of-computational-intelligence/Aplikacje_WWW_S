540c8dee10cbc83694e6dabae4f55fbeb4be1a18
wicios007 <aa@bb.cc>
2021-02-24 19:05:02 +0100

    Lab1 Commit1

---
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+namespace SchoolRegister.BLL.DataModels
+{
+    public class Role : IdentityRole<int>
+    {
+        public RoleValue RoleValue { get; set; }
+        public Role() { }
+        public Role(string name, RoleValue value) : base(name)
+        {
+            this.RoleValue = value;
+        }
+    }
+}
