79b1774350cf2a2d27fbf9ec594873cf04a67d0f
Lukasz Cz <luskszkskskzakakczcz@gmail.com>
2021-02-24 11:29:25 +0100

    Commit 1

---
+++ SchoolRegister.Model/DataModels/GradeScale.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SchoolRegister.BLL.DataModels
+{
+    public enum GradeScale { NDST, DST, DB, BDB };
+}
---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SchoolRegister.BLL.DataModels
+{
+    public enum RoleValue { User, Student, Parent, Teacher, Admin };
+}
