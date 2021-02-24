8bc571964a68d874ba21dab1f2079ced420c3b4f
Brzeziu <brzeziu@interia.pl>
2021-02-24 17:01:16 +0100

    Revert "lab1"
    This reverts commit c6e4c21afd252d066e9c36f21876b8ea2028996e.

--- SchoolRegister.Model/DataModels/Parent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace SchoolRegister.BLL.DataModels
-{
-    public class Parent : User
-    {
-        public IList<Student> Students { get; set; }
-    }
-}
--- SchoolRegister.Model/DataModels/RoleValue.cs
+++
@@ -1,7 +0,0 @@
-namespace SchoolRegister.BLL.DataModels
-{
-    public enum RoleValue
-    {
-        User, Student, Parent, Teacher, Admin
-    }
-}
