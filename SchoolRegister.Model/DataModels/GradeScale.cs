91aa94328698f371fa63efb611ced8e9d8a90051
KamilSikora <kamilsikora1316@gmail.com>
2021-02-26 22:54:16 +0100

    Lab1

---
+++ SchoolRegister.Model/DataModels/GradeScale.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum GradeScale : int
+    {
+        NDST=2, DST=3, DB=4, BDB=5
+    }
+}
---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum RoleValue
+    {
+        User, Student, Parent, Teacher, Admin
+    }
+}
