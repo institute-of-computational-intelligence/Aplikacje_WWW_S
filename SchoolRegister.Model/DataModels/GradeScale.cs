27517b8cba0b4b85e3b9d51ee8159c5da6bb148c
KonradBarszczyk <kbarszczyk@protonmail.com>
2021-03-03 20:00:17 +0100

    Add solve for first laboratory

---
+++ SchoolRegister.Model/DataModels/GradeScale.cs
@@ -0,0 +1,8 @@
+namespace SchoolRegister.Model.DataModels{
+    public enum GradeScale:ushort{
+        NDST=2,
+        DST=3,
+        DB=4,
+        BDB=5
+    }
+}
---
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels{
+
+    public class Parent{
+        IList<Student> Students{get;set;}
+    }
+}
