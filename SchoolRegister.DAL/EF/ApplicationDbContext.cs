7c76543620c900663e3dd9eafc012486e550fbe9
Roksana <roksana@mail.com>
2021-03-03 12:26:07 +0100

    Lab2 - complete to point 2.4

---
+++ SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.Model.DataModels;
+
+namespace SchoolRegister.DAL.EF
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    {
+        // Table properties e.g
+        // public virtual DbSet<Entity> TableName { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            // configuration commands
+            optionsBuilder.UseLazyLoadingProxies(); // enable lazy loading proxies
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Fluent API commands
+            object p = modelBuilder.Entity<User>()
+                .ToTable("AspNetUsers")
+                .HasDiscriminator<int>("UserType")
+                .HasValue<User>((int)RoleValue.User)
+                .HasValue<Student>((int)RoleValue.Student)
+                .HasValue<Parent>((int)RoleValue.Parent)
+                .HasValue<Teacher>((int)RoleValue.Teacher);
+        }
+    }
+}
