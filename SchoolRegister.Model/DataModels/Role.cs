a813314ac955528f8de9eec6927480bef35f4a18
Michał Holak <michalholak98@gmail.com>
2021-03-03 14:54:18 +0100

    Lab 1

---
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Role : IdentityRole<int>
+    {
+       public RoleValue RoleValue { get; set; }
+
+       public Role() {}
+       public Role(string name, RoleValue roleValue)
+       {
+           base.Id = (int)roleValue;
+           base.Name = name;
+           
+       }
+ 
+    }
+
+}
