0835537685f9fd84af34bd26bc0ca4fe48203209
adrianniglus <adrian.niglus@gmail.com>
2021-03-04 21:46:18 +0100

    Lab01

---
+++ SchoolRegister.Model/DataModels/Grade.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace SchoolRegister.BLL.DataModels
+{
+    public class Grade
+    {
+        public DateTime DateOfIssue {get; set;}
+        public GradeScale GradeValue {get; set;}
+        public Student Student {get; set;}
+        public int StudentId {get;set;}
+        public Subject Subject {get; set;}
+        public int SubjectId {get; set;}
+    }
+}
