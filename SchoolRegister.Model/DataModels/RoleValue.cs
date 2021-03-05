80bce01c26f3d14fed893c6ceda64b9ff7e51066
Patryk Stępień <patryk1stepien@gmail.com>
2021-03-05 13:22:27 +0100

    LAb-1

---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,18 @@
+  
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels
+{
+
+    public enum RoleValue
+    {
+        User,
+        Student,
+        Parent,
+        Teacher,
+        Admin,
+    }
+    
+}
---
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Student
+    {
+        public double AverageGrade { get; }
+        public IDictionary<string, double> AverageGradeOerSubject { get; }
+        public IList<Grade> Grades { get; set; }
+        public IDictionary<string, List<GradeScale>> GradesPerSubject { get; }
+        public Group Group { get; set; }
+        public int? GroupId { get; set; }
+        public Parent Parent { get; set; }
+        public int? ParentId { get; set; }
+
+    }
+
+}
