4c53003b4ccbcb7a37fa33a8eafbae46a2a85600
adam <das@gmail.com>
2021-03-07 14:46:28 +0100

    Lab 1

---
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Student: User
+    {
+        public double AverageGrade{get;}
+        public IDictionary<string,double> AvarageGradePerSubject {get;}
+        public IList<Grade> Grades {get;} 
+        public IDictionary<string,List<GradeScale>> GradesPerSubject {get;}
+        
+        public Group Group {get;set;}
+
+        public int? GroupId {get;set;}
+
+        public Parent Parent {get;set;} 
+        public int? ParentId  {get;set;} 
+     }
+
+
+
+}
