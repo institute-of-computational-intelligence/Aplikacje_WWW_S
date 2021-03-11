f23c6c9da1be3737398b513726d50295650183d2
MichałKucharski <email@smth.smth>
2021-03-11 20:52:42 +0100

    Lab1

---
+++ SchoolRegister.Model/DataModels/GradeScale.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum GradeScale
+    {
+        NDST,
+        DST,
+        DB,
+        BDB
+
+    }
+}
