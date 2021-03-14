ac610b69fba00d5f5afbf6e5e061026271406827
Przemyslaw <kaminskiprzemyslaw1@gmail.com>
2021-03-14 12:12:23 +0100

    Lab 1

---
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+namespace SchoolRegister.Model.DataModels
+{
+    public class Role :IdentityRole<int>
+    {
+        RoleValue RoleValue {get;set;}
+        public Role()
+        {
+
+        }
+        public Role(string name,RoleValue roleValue)
+        {
+
+        }
+    }
+
+
+}
---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,11 @@
+namespace SchoolRegister.Model.DataModels
+{
+    public enum RoleValue
+    {
+        User = 1,
+        Student = 2,
+        Parent = 3,
+        Teacher = 4,
+        Admin = 5
+    }
+}
