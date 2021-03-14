8b1e17562ad009330f44f87b030c58393532087e
lsphony <plech0810@gmail.com>
2021-03-14 19:43:58 +0100

    lab1

---
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Parent : User
+    {
+        public IList<Student> Students { get; set; }
+
+    }
+
+
+}
---
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Role : IdentityRole<int>
+    {
+        public RoleValue RoleValue { get; set; }
+
+        private Role()
+        {
+
+        }
+
+        private Role(string name, RoleValue roleValue)
+        {
+
+        }
+    }
+}
