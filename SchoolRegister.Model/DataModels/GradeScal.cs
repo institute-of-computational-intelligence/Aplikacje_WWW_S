281fa2eb12cf2bab04a5edd927a5c1f252af035e
Ernest <tlustochowicz333@interia.pl>
2021-03-17 13:59:57 +0100

    lab1

---
+++ SchoolRegister.Model/DataModels/GradeScal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum GradeScale
+    {
+        NDST,
+        DST,
+        DB,
+        BDB
+    }
+}
---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum RoleValue
+    {
+        User,
+        Student,
+        Parent,
+        Teacher,
+        Admin
+    }
+}
