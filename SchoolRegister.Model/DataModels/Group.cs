7a76d0223f621afc48c1c31dbcbdf7cd17dfe186
Sciegien <sciegn4pl@gmail.com>
2021-03-22 15:51:42 +0100

    Piotr Ściegienka

--- SchoolRegister.Model/DataModels/Group.cs
+++ SchoolRegister.Model/DataModels/Group.cs
@@ -2,13 +2,18 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace SchoolRegister.Model.DataModels
 {
     public class Group
     {
+
+        [Key]
         public int Id {get; set;}
+        [Required]
         public string Name {get; set;}
         public IList<Student> Students {get; set;}
         public IList<SubjectGroup> SubjectGroups {get; set;}
--- SchoolRegister.Model/DataModels/Teacher.cs
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolRegister.Model.DataModels
 {
     public class Teacher : User
     {
-        public IList<Subject> Subjects { get; set; }
+        public virtual IList<Subject> Subjects { get; set; }
+        [Required]
         public string Title { get; set; }
     }
 }
