ffbe897c6049524d8cd21aa6d528e2cd30955a97
Patryk Stępień <patryk1stepien@gmail.com>
2021-03-22 15:53:36 +0100

    skonczone

--- SchoolRegister.Model/DataModels/Teacher.cs
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolRegister.Model.DataModels
 {
@@ -9,7 +11,8 @@ namespace SchoolRegister.Model.DataModels
     public class Teacher : User
     {
 
-        public IList<Subject> Subjects { get; set; }
+        public virtual  IList<Subject> Subjects { get; set; }
+        [Required]
         public string Title { get; set; }
     }
     
