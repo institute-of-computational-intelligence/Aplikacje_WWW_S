a7e264d2e78cdaa389e5fd55355db815a435852d
MateuszSzpaczek <mateusz14-900@o2.pl>
2021-03-28 19:25:25 +0200

    Lab2

--- SchoolRegister.Model/DataModels/Parent.cs
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 
 namespace SchoolRegister.Model.DataModels{
-    public class Parent
+    public class Parent : User
     {
         public virtual IList<Student> Students{get;set;}
     }
