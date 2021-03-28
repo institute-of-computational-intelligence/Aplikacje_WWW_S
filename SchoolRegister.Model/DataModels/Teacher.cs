bf89c71b7774ff1a85f40291111a717c38f78492
norbi <bobivankenobi@onet.pl>
2021-03-28 23:10:00 +0200

    Lab 2

--- SchoolRegister.Model/DataModels/Teacher.cs
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolRegister.Model.DataModels
 {
     public class Teacher : User
     {
-        public IList<Subject> Subcjects {get; set;}
+        public virtual IList<Subject> Subcjects {get; set;}
+        [Required]
         public string Title {get; set;}
         
     }
