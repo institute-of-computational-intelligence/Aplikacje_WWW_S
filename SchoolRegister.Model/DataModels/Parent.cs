324b49b8825a7564326d7e3a5b5b2c4b46dbda84
DamianRękorajski <francisco2k10@gmail.com>
2021-03-28 16:33:00 +0200

    lab2

--- SchoolRegister.Model/DataModels/Parent.cs
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 
 namespace SchoolRegister.Model.DataModels
 {
-     public class Parent : User
-     {
-         public IList<Student> Students { get; set; }
+    public class Parent: User
+    {
+        public virtual IList<Student> Students{get;set;}
      }
+
 }
--- SchoolRegister.Model/DataModels/Role.cs
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -1,20 +1,18 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace SchoolRegister.Model.DataModels
 {
-     public class Role : IdentityRole<int>
-     {
-         public Role()
-         {
+    public class Role : IdentityRole<int>
+    {
+        public RoleValue RoleValue { get; set; }
+        public Role()
+        {
 
-         }       
-
-         public Role(string name, RoleValue roleValue)
-         {
-            
-         }
-
-         public RoleValue RoleValue { get; set; }
-    
-     }
+        }
+        public Role(string name, RoleValue roleValue)
+        {
+            this.RoleValue = roleValue;
+        }
+    }
 }
