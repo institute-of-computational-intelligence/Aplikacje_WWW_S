ffd714a6a5ba9b93fe6098540e17184e7f9dacbb
Adrian <jicer1242@gmail.com>
2021-03-28 22:43:33 +0200

    x

--- SchoolRegister.Model/DataModels/SubjectGroup.cs
+++ SchoolRegister.Model/DataModels/SubjectGroup.cs
@@ -1,15 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SchoolRegister.Model.DataModels
 {
     public class SubjectGroup
     {
-        public Group Group { get; set; }
-        public int GroupId { get; set; }
-        public Subject Subject { get; set; }
-        public int SubjectId { get; set; }
+        public virtual Group Group {get; set;}
+        public int GroupId {get; set;}
+        public virtual Subject Subject {get; set;}
+        public int SubjectId {get; set;}
     }
 }
--- SchoolRegister.Model/DataModels/Teacher.cs
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolRegister.Model.DataModels
 {
     public class Teacher : User
     {
-        public IList<Subject> Subjects { get; set; }
+        public virtual IList<Subject> Subjects { get; set; }
+        [Required]
         public string Title { get; set; }
     }
 }
