e6b34eff6f73e20995ee0f93183583ea1df29c84
Daniel Więcek <daniel.wiecek@git.pl>
2021-04-08 20:16:11 +0200

    Lab3

---
+++ SchoolRegister.Services/Interfaces/ITeacherService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Identity;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.ViewModels.VM;
+
+namespace SchoolRegister.Services.Interfaces
+{
+    public interface ITeacherService
+    {
+        void AddGradeAsync(AddGradeAsyncVm addGradeVm);
+        void SendEmailToParent(SendEmailVm sendEmailVm);
+    }
+}
