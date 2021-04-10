58d937293e4aeeb9919d5dd2ace8c2bcf08b46cd
Roksana <roksana@mail.com>
2021-04-10 17:13:19 +0200

    Lab3 - fixed bug with mapping

--- SchoolRegister.Web/Configuration/Profiles/MainProfile.cs
+++ SchoolRegister.Web/Configuration/Profiles/MainProfile.cs
@@ -12,7 +12,8 @@ namespace SchoolRegister.Web.Configuration.Profiles
             //AutoMapper maps
             CreateMap<Subject, SubjectVm>() // map from Subject(src) to SubjectVm(dst)
                 // custom mapping: FirstName and LastName concat string to TeacherName
-                .ForMember(dest => dest.TeacherName, x => x.MapFrom(src => $"{src.Teacher.FirstName} {src.Teacher.LastName}"))
+                .ForMember(dest => dest.TeacherName, x => x.MapFrom(src => $"{src.Teacher.Firstname} {src.Teacher.LastName}"))
+                // Firstname insted of FirstName because in SchoolRegister.DAL>>Migrations>>ApplicationDbContextModelSnapshot.cs is this name
                 // custom mapping: ILIst<Group> to IList<GroupVm>
                 .ForMember(dest => dest.Groups, x => x.MapFrom(src => src.SubjectGroups.Select(y => y.Group)));
 
