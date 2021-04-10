3a71b4352c33a89af8bc190957ea93f6b5b8e838
Roksana <roksana@mail.com>
2021-04-10 16:59:19 +0200

    Lab3 - complete

---
+++ SchoolRegister.ViewModels/VM/AddToGroupVm.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class AddToGroupVm
+    {
+        [Required]
+        public int GroupId { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+    }
+} 
