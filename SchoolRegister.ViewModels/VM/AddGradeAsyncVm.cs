ac1c6d1758c5dadc079b98d55a9ca865fc90d2f8
Mateusz <zkmateusz@tuta.io>
2021-04-11 17:20:05 +0200

    Lab3

---
+++ SchoolRegister.ViewModels/VM/AddGradeAsyncVm.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using SchoolRegister.Model.DataModels;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class AddGradeAsyncVm
+    {
+        [Required]
+        public int StudentId{ get; set; }
+        
+        [Required]
+        public int TeacherId { get; set; }
+
+        [Required]
+        public int SubjectId { get; set; }
+
+        [Required]
+        public GradeScale GradeValue { get; set; }
+    }
+}
---
+++ SchoolRegister.ViewModels/VM/AddGroupVm.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class AddGroupVm
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
