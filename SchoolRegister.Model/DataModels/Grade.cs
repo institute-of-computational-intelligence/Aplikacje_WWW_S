7f29b235987a96fe6fcca1ae1d831cb83ed77846
norbi <bobivankenobi@onet.pl>
2021-04-13 14:02:47 +0200

    Lab3

--- SchoolRegister.Model/DataModels/Grade.cs
+++ SchoolRegister.Model/DataModels/Grade.cs
@@ -9,14 +9,14 @@ namespace SchoolRegister.Model.DataModels
 
         [Key]
 
-        public DateTime DateOfIssue {get; set;}
-        public GradeScale GradeValue {get; set;}
-        public virtual Student Student {get; set;}
+        public DateTime DateOfIssue { get; set; }
+        public GradeScale GradeValue { get; set; }
+        public virtual Student Student { get; set; }
         [ForeignKey("Student")]
-        public int StudentId {get; set;}
-        public virtual Subject Subject {get; set;}
+        public int StudentId { get; set; }
+        public virtual Subject Subject { get; set; }
         [ForeignKey("Subject")]
-        public int SubjectId {get; set;}
+        public int SubjectId { get; set; }
 
     }
 }
