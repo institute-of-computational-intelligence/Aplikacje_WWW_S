4a6a2232d0f7116e6e65f5c45ebb227608f5c052
Ihor Davydenko <a123asdasd@gmail.com>
2021-04-25 17:40:51 +0200

    lab3

---
+++ SchoolRegister.Services/Interfaces/IStudentService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.ViewModels.VM;
+
+namespace SchoolRegister.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        void AddToGroupAsync(AddToGroupVm addToGroupVm);
+        void RemoveFromGroupAsync(RemoveFromGroupVm removeFromGroupVm);
+    }
+} 
