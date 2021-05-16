af8367ed03ee984ff65503957e031efaec156300
Mykhailo Bazyshyn <mb@gmail.com>
2021-05-16 18:54:19 +0200

    lab4

---
+++ SchoolRegister.ViewModels/VM/CheckGradesVm.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class CheckGradesVm
+    {   
+        [Required]
+        public int CurrentUserId { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+    }
+} 
