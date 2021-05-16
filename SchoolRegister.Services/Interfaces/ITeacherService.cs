f5c3ee821df1bc81a2d7fc04129dc1481ed7aa83
Ihor Davydenko <a123asdasd@gmail.com>
2021-05-16 18:14:07 +0200

    Lab4

---
+++ SchoolRegister.Services/Interfaces/ITeacherService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.ViewModels.VM;
+
+namespace SchoolRegister.Services.Interfaces
+{
+    public interface ITeacherService
+    {
+        void AddGradeAsync(AddGradeAsyncVm addGradeVm);
+        void SendEmailToParent(SendEmailVm sendEmailVm);
+    }
+}
