347e6d255e43641ba2c088913f7d93048ad9ec63
Sciegien <sciegn4pl@gmail.com>
2021-06-05 20:11:54 +0200

    Lab07

--- SchoolRegister.Services/Interfaces/ISubjectService.cs
+++ SchoolRegister.Services/Interfaces/ISubjectService.cs
@@ -11,5 +11,7 @@ namespace SchoolRegister.Services.Interfaces
         SubjectVm AddOrUpdateSubject(AddOrUpdateSubjectVm addOrUpdateVm);
         SubjectVm GetSubject(Expression<Func<Subject, bool>> filterExpression);
         IEnumerable<SubjectVm> GetSubjects(Expression<Func<Subject, bool>> filterExpression = null);
+
+        bool RemoveSubject(Expression<Func<Subject, bool>> filterExpression);
     }
-}
+} 
