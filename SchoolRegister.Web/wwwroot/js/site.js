5200a6f9af8e8500a877e65cd988b388b80076d5
Roksana <roksana@mail.com>
2021-06-09 12:02:17 +0200

    Lab8

--- SchoolRegister.Web/wwwroot/js/site.js
+++ SchoolRegister.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(".subjects-filter-input").change(async function () {
+    const filterValue = $(".subjects-filter-input").val();
+    const result = await $.get("/Subject/Index", $.param({ filterValue: filterValue }));
+    $(".subjects-table-data").html(result);
+});
