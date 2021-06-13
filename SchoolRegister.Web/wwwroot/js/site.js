325e4f4b0a5880df05e698dceb7b2eaac16b9370
Konrad Międła <konrad.miedla@gmail.com>
2021-06-13 18:57:27 +0200

    Lab8

--- SchoolRegister.Web/wwwroot/js/site.js
+++ SchoolRegister.Web/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$(".subjects-filter-input").change(async function () {
+    const filterValue = $(".subjects-filter-input").val();
+    const result = await $.get("/Subject/Index", $.param({ filterValue: filterValue }));
 
-// Write your JavaScript code.
+    $(".subjects-table-data").html(result);
+});
+
+$(".students-filter-input").change(async function () {
+    const filterValue = $(".students-filter-input").val();
+    const result = await $.get("/Student/Index", $.param({ filterValue: filterValue }));
+
+    $(".students-table-data").html(result);
+});
